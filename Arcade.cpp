b305fb4a38ef6f9f86dd3513c0fb4fdce353c9cb
Nixan <43931275+nixangauli@users.noreply.github.com>
2023-05-15 17:02:02 +0545

    Update Arcade.cpp

--- Arcade.cpp
+++ Arcade.cpp
@@ -1,4 +1,4 @@
-﻿#include <raylib.h>
+#include <raylib.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -36,7 +36,6 @@ private:
     const float moveSpeed = 300.0f;
     const float jumpForce = 600.0f;
     Vector2 velocity;
-    Vector2 position;
     bool isJumping;
 
 public:
@@ -45,8 +44,6 @@ public:
         , velocity({ 0.0f, 0.0f })
         , isJumping(false)
     {
-        position.x = 0;  // Set the x-coordinate to the left side of the screen
-        position.y = screenHeight / 2;  // Set the y-coordinate to the center of the screen
     }
 
     void Update() override
@@ -69,7 +66,7 @@ public:
         }
 
         // Apply gravity
-        velocity.y += 1000.0f * GetFrameTime();
+        velocity.y += 1500.0f * GetFrameTime();
 
         // Apply velocity to position
         position.y += velocity.y * GetFrameTime();
@@ -101,11 +98,15 @@ class Game {
 private:
     Player player;
     std::vector<Platform> platforms;
+    Texture2D background;
 
 public:
     Game()
-        : player({ screenWidth / 2.0f, screenHeight / 2.0f })
+        : player({ 50.0f, screenHeight / 2.0f })
     {
+        // Load the background image
+        background = LoadTexture("Images/Background.png");
+
         platforms.push_back(Platform({ 0, screenHeight - 20 }, "Images/Mainplatform.png"));
         platforms.push_back(Platform({ screenWidth / 2.0f, screenHeight - 20 }, "Images/Mainplatform.png"));
         platforms.push_back(Platform({ screenWidth - 100, screenHeight - 20 }, "Images/Mainplatform.png"));
@@ -127,12 +128,45 @@ public:
 
         ClearBackground(RAYWHITE);
 
+        // Draw the background image
+        DrawTexture(background, 0, 0, WHITE);
+
         player.Draw();
 
         for (auto& platform : platforms)
         {
             platform.Draw();
         }
+
+        EndDrawing();
+    }
+};
+
+class CreditScreen {
+public:
+    void Draw() const {
+        BeginDrawing();
+
+        ClearBackground(LIME);
+
+        // Calculate the center position of the screen
+        int centerX = screenWidth / 2;
+        int centerY = screenHeight / 2;
+
+        // Calculate the position of the credit text
+        int textWidth = MeasureText("Credits", 40);
+        int textHeight = 40;
+        int textX = centerX - textWidth / 2;
+        int textY = centerY - textHeight / 2;
+
+        // Draw the credit text
+        DrawText("Credits", textX, textY - 150, 50, BLACK);
+        DrawText("Developers:", textX, textY - 100, 40, BLACK);
+        DrawText("Nixan Gauli", textX, textY - 50, 25, BLACK);
+        DrawText("Nitesh Poudel", textX, textY, 25, BLACK);
+        DrawText("Utsav Thapa", textX, textY + 50, 25, BLACK);
+        DrawText("Artworks", textX, textY + 100, 50, BLACK);
+        DrawText("Google", textX, textY + 150, 25, BLACK);
 
         EndDrawing();
     }
@@ -183,9 +217,9 @@ public:
         int textY = centerY - textHeight / 2;
 
         // Draw menu items
-        DrawText("Start Game", textX, textY-50, 30, selectedMenuItem == MenuItem::StartGame ? BLUE : BLACK);
+        DrawText("Start Game", textX, textY - 50, 30, selectedMenuItem == MenuItem::StartGame ? BLUE : BLACK);
         DrawText("Credits", textX, textY, 30, selectedMenuItem == MenuItem::Credits ? BLUE : BLACK);
-        DrawText("Exit", textX, textY+50, 30, selectedMenuItem == MenuItem::Exit ? BLUE : BLACK);
+        DrawText("Exit", textX, textY + 50, 30, selectedMenuItem == MenuItem::Exit ? BLUE : BLACK);
 
         EndDrawing();
     }
@@ -199,9 +233,11 @@ int main()
     InitWindow(screenWidth, screenHeight, "Asylum");
     Menu menu;
     Game game;
+    CreditScreen credit;
 
     bool inMainMenu = true;
     bool inGame = false;
+    bool inCredits = false;
 
     while (!WindowShouldClose()) {
         if (inMainMenu) {
@@ -216,6 +252,8 @@ int main()
                     break;
                 case Menu::MenuItem::Credits:
                     // Show credits
+                    inMainMenu = false;
+                    inCredits = true;
                     break;
                 case Menu::MenuItem::Exit:
                     CloseWindow();
@@ -230,6 +268,14 @@ int main()
             if (IsKeyPressed(KEY_ESCAPE)) {
                 inMainMenu = true;
                 inGame = false;
+            }
+        }
+        else if (inCredits) {
+            credit.Draw();
+
+            if (IsKeyPressed(KEY_ESCAPE)) {
+                inMainMenu = true;
+                inCredits = false;
             }
         }
     }
